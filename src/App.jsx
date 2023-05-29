ab4b854d40f098e1a221150298209cc368c59a12
LevaniMigriauli <levanimigriauli16@gmail.com>
2023-05-29 19:44:37 +0400

    working on taskUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React, { useRef, useState } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import "./App.css";
 
 import iconDelete from "./assets/delete.svg";
+import iconUpdate from "./assets/update.svg";
+import iconSave from "./assets/save.svg";
 
 function App() {
   // const [taskInputValue, setTaskInputValue] = useState();
-  const taskInputValue = useRef();
+  const taskInputValue = useRef("");
   const [tasks, setTasks] = useState([]);
+
+  const [taskUpdater, setTaskUpdater] = useState(false);
+  const [taskupdatedValue, setTaskupdatedValue] = useState("");
 
   const taskAddHandler = function () {
     const obj = {
@@ -16,6 +21,8 @@ function App() {
     setTasks((prevState) => {
       return [...prevState, obj];
     });
+
+    taskInputValue.current.value = "";
 
     // localStorage.setItem("task", JSON.stringify(tasks));
   };
@@ -29,9 +36,17 @@ function App() {
           break;
         }
       }
-
       return [...prevState];
     });
+  };
+
+  const taskUpdateHandler = function (task) {
+    setTaskUpdater(true);
+    console.log(taskupdatedValue);
+  };
+
+  const taskSaveHandler = function () {
+    setTaskUpdater(false);
   };
 
   // console.log(tasks);
@@ -40,6 +55,7 @@ function App() {
     <>
       <div>
         <input
+          id="task-input"
           type="text"
           name=""
           id="add"
@@ -56,11 +72,38 @@ function App() {
         </button>
       </div>
       <div>
-        <ul>
+        <ul className="tasks-list">
           {tasks.map((task) => (
-            <li key={task.id} onClick={() => taskDeleteHandler(task)}>
-              {task.value}
-              <img src={iconDelete} alt="" />
+            <li key={task.id}>
+              {taskUpdater ? (
+                <Fragment>
+                  <input defaultValue={task.value} />
+                  <button onClick={taskSaveHandler}>
+                    <img src={iconSave} alt="" />
+                  </button>
+                </Fragment>
+              ) : (
+                <Fragment>
+                  <span>{task.value}</span>
+
+                  <button>
+                    <img
+                      className="edit"
+                      src={iconUpdate}
+                      alt="Delete icon"
+                      onClick={() => taskUpdateHandler(task)}
+                    />
+                  </button>
+                  <button>
+                    <img
+                      className="delete"
+                      src={iconDelete}
+                      alt="Delete icon"
+                      onClick={() => taskDeleteHandler(task)}
+                    />
+                  </button>
+                </Fragment>
+              )}
             </li>
           ))}
         </ul>
