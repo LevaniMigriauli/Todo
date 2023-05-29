f997a6cd5cc89bef2d66c0dcf8651b460f31c02d
LevaniMigriauli <levanimigriauli16@gmail.com>
2023-05-29 12:31:51 +0400

    task add functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  // const [taskInputValue, setTaskInputValue] = useState();
+  const taskInputValue = useRef();
+  const [tasks, setTasks] = useState([]);
+
+  const taskAddHandler = function () {
+    const obj = {
+      id: String(Math.random()),
+      value: taskInputValue.current.value,
+    };
+    setTasks((prevState) => [...prevState, obj]);
+  };
+
+  console.log(tasks);
+
   return (
     <>
-      <p>We</p>
+      <div>
+        <input
+          type="text"
+          name=""
+          id="add"
+          ref={taskInputValue}
+          // value={taskInputValue}
+          // onChange={(e) => setTaskInputValue(e.target.value)}
+          placeholder="Note"
+        />
+        <button className="btn-add" onClick={taskAddHandler}>
+          +
+        </button>
+      </div>
+      <div>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>{task.value}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
